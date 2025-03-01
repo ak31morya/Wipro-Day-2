7b57172a04c6d49186bf7bce562963b0a0c8820c
Aashish Morya <89730571+ak31morya@users.noreply.github.com>
2025-03-01 16:25:54 +0530

    Add files via upload

---
+++ expr.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+app.get("/", (req, res) => {
+    res.send("My First Service in Express JS...");
+})
+
+app.get("/aashish", (req,res) => {
+    res.send("Hi I am Aashish from Wipro...");
+})
+
+app.get("/show/:name", (req, res) => {
+    const name = req.params.name;
+    res.send("Hi Welcome to " +name);
+})
+
+app.listen(1111, (req, res) => {
+    console.log("Node Js Application Started...");
+})
